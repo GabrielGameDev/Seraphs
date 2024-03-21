72d193919a03b26b27df9b17d2ca10250cd08406
GabrielGameDev <gabrielgamedev@gmail.com>
2024-03-21 14:27:01 -0300

    Dano no inimigo

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public float maxHealth = 12;
+	public ParticleSystem damageEffect;
+	float currentHealth;
+	SpriteRenderer spriteRenderer;
+
+	private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		currentHealth = maxHealth;
+	}
+
+	public void TakeDamage(float damage, Vector2 position)
+	{
+		currentHealth -= damage;
+		Color color = spriteRenderer.color;
+		color.a = currentHealth / maxHealth;
+		spriteRenderer.color = color;
+		damageEffect.transform.position = position;
+		damageEffect.Play();
+		
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		
+		damageEffect.transform.parent = null;
+		damageEffect.transform.localScale = new Vector3(1, 1, 1);
+		Destroy(damageEffect.gameObject, damageEffect.main.duration);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/TiroPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroPlayer : MonoBehaviour
+{
+    public float damage = 4;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+			enemy.TakeDamage(damage, transform.position);
+			Destroy(gameObject);
+		}
+		
+	}
+}
